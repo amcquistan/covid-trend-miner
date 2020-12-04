d87801ff17e15850ae8eda9f4e7a3973c33b94c9
Adam McQuistan <sci.guy.mcq@gmail.com>
2020-12-04 14:05:09 -0600

    systemd scripts and configs for airflow

---
+++ etl-pipeline/airflow-systemd.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# exit when any command fails and echo out each command
+set -ex
+
+USER=$(whoami)
+
+if [[ $USER -neq "etl" ]]; then
+  printf "\n\n!!!Exiting this is meant to be ran by etl user\n"
+  exit 1
+fi
+
+cd /home/etl/covid-trend-miner
+
+cp airflow-pid.conf /etc/tmpfiles.d/airflow-pid.conf
+
+cp airflow-systemd.conf /etc/airflow-systemd.conf
+
+cp airflow-webserver.service /etc/systemd/system/airflow-webserver.service
+cp airflow-scheduler.service /etc/systemd/system/airflow-scheduler.service
+
+
+
+
+
+
+
