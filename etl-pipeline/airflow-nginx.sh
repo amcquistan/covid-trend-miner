ee805056e4ccd28c0bfaaaf27c7eba1e21291ffd
Adam McQuistan <sci.guy.mcq@gmail.com>
2020-12-04 21:57:03 -0600

    postgresql config fix

--- etl-pipeline/airflow-nginx.sh
+++ etl-pipeline/airflow-nginx.sh
@@ -16,3 +16,5 @@ sudo cp airflow-nginx.conf /etc/nginx/sites-available/airflow-nginx.conf
 sudo ln -s /etc/nginx/sites-available/airflow-nginx.conf /etc/nginx/sites-enabled/airflow-nginx.conf
 
 sudo unlink /etc/nginx/sites-enabled/default
+
+# need to update base_url = http://ec2-3-128-133-211.us-east-2.compute.amazonaws.com/airflow in /home/etl/airflow/airflow.cfg
