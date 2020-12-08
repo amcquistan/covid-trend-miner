d039bb2118d2c6cd52399cadb16f52a5fe4342a0
Adam McQuistan <sci.guy.mcq@gmail.com>
2020-12-07 18:36:53 -0600

    finishing touches

--- rest-api/flask_api/app.py
+++ rest-api/flask_api/app.py
@@ -7,6 +7,9 @@ import psycopg2
 from psycopg2 import extras
 
 
+app = FlaskLambda(__name__)
+
+
 def connect_to_db():
     # create db connection
 
@@ -28,13 +31,14 @@ def connect_to_db():
     print('Connected to db')
     return db
 
-app = FlaskLambda(__name__)
 
 @app.route('/countries/')
 def fetch_countries():
     sql = """
-        SELECT DISTINCT location_id, country FROM location_dim WHERE state IS NULL
-        AND city IS NULL ORDER BY country;
+        SELECT DISTINCT location_id, country 
+        FROM location_dim 
+        WHERE state IS NULL
+            AND city IS NULL ORDER BY country;
         """
     db = connect_to_db()
     cursor = db.cursor(cursor_factory=extras.DictCursor)
@@ -43,43 +47,95 @@ def fetch_countries():
     return jsonify(countries)
 
 
-# @app.route('/countries/<int:country_id>')
-# def fetch_country(country_id):
-#     sql = """
-#         SELECT DISTINCT f.date_id, f.location_id, cases, recoveries, deaths, cases_100k, testing_rate, hospitalization_rate,
-#         date, year, month, day_of_week, day_of_month,
-#         country, state, city, latitude, longitude, population
-#         FROM covid_facts f JOIN date_dim d ON d.date_id = f.date_id
-#         JOIN location_dim l ON l.location_id = f.location_id
-#         WHERE f.location_id = %s;
-#         """
+def to_float(data, key):
+    val = data.get(key, None)
+    if val:
+        return float(val)
+    return None
 
 
-# @app.route('/states')
-# def fetch_states():
-#     sql = """
-#         SELECT DISTINCT location_id, state FROM location_dim WHERE city IS NULL ORDER BY state;
-#         """
+def normalize_datatypes(data):
+    dc = dict(data)
+    dc['cases_100k'] = to_float(dc, 'cases_100k')
+    dc['testing_rate'] = to_float(dc, 'testing_rate')
+    dc['hospitalization_rate'] = to_float(dc, 'hospitalization_rate')
+    dc['latitude'] = to_float(dc, 'latitude')
+    dc['longitude'] = to_float(dc, 'longitude')
+
+    return dc
 
 
-# @app.route('/states/<int:state_id>')
-# def fetch_state(state_id):
-#     sql = """
-#         SELECT DISTINCT f.date_id, f.location_id, cases, recoveries, deaths, cases_100k, testing_rate, hospitalization_rate,
-#         date, year, month, day_of_week, day_of_month,
-#         country, state, city, latitude, longitude, population
-#         FROM covid_facts f JOIN date_dim d ON d.date_id = f.date_id
-#         JOIN location_dim l ON l.location_id = f.location_id
-#         WHERE f.location_id = %s;
-#         """
+@app.route('/countries/<int:country_id>/')
+def fetch_country(country_id):
+    sql = """
+        SELECT DISTINCT f.date_id, f.location_id, cases, recoveries, deaths, cases_100k, testing_rate, hospitalization_rate,
+        date, year, month, day_of_week, day_of_month,
+        country, state, city, latitude, longitude, population
+        FROM covid_facts f JOIN date_dim d ON d.date_id = f.date_id
+        JOIN location_dim l ON l.location_id = f.location_id
+        WHERE f.location_id = %s
+        """
 
-# @app.route('/cities')
-# def fetch_cities():
-#     sql = """
-#         SELECT DISTINCT location_id, city FROM location_dim ORDER BY state;
-#         """
+    db = connect_to_db()
+    cursor = db.cursor(cursor_factory=extras.DictCursor)
+    cursor.execute(sql, (country_id, ))
+    days = [normalize_datatypes(row) for row in cursor.fetchall()]
+    return jsonify(days)
 
-# @app.route('/cities/<int:city_id>')
-# def fetch_city(city_id):
-#     sql = """
-#         """
+
+@app.route('/states/')
+def fetch_states():
+    sql = """
+        SELECT DISTINCT location_id, state FROM location_dim WHERE city IS NULL ORDER BY state;
+        """
+    db = connect_to_db()
+    cursor = db.cursor(cursor_factory=extras.DictCursor)
+    cursor.execute(sql)
+    states = [dict(row) for row in cursor.fetchall()]
+    return jsonify(states)
+
+
+@app.route('/states/<int:state_id>/')
+def fetch_state(state_id):
+    sql = """
+        SELECT DISTINCT f.date_id, f.location_id, cases, recoveries, deaths, cases_100k, testing_rate, hospitalization_rate,
+        date, year, month, day_of_week, day_of_month,
+        country, state, city, latitude, longitude, population
+        FROM covid_facts f JOIN date_dim d ON d.date_id = f.date_id
+        JOIN location_dim l ON l.location_id = f.location_id
+        WHERE f.location_id = %s;
+        """
+    db = connect_to_db()
+    cursor = db.cursor(cursor_factory=extras.DictCursor)
+    cursor.execute(sql, (state_id, ))
+    days = [normalize_datatypes(row) for row in cursor.fetchall()]
+    return jsonify(days)
+
+
+@app.route('/cities/')
+def fetch_cities():
+    sql = """
+        SELECT DISTINCT location_id, city FROM location_dim ORDER BY city;
+        """
+    db = connect_to_db()
+    cursor = db.cursor(cursor_factory=extras.DictCursor)
+    cursor.execute(sql)
+    cities = [dict(row) for row in cursor.fetchall()]
+    return jsonify(cities)
+
+
+@app.route('/cities/<int:city_id>/')
+def fetch_city(city_id):
+    sql = """
+        SELECT DISTINCT f.date_id, f.location_id, cases, recoveries, deaths, cases_100k, testing_rate, hospitalization_rate,
+        date, year, month, day_of_week, day_of_month,
+        country, state, city, latitude, longitude, population
+        FROM covid_facts f JOIN date_dim d ON d.date_id = f.date_id
+        JOIN location_dim l ON l.location_id = f.location_id
+        WHERE f.location_id = %s;
+        """
+    db = connect_to_db()
+    cursor = db.cursor(cursor_factory=extras.DictCursor)
+    cursor.execute(sql, (city_id, ))
+    days = [normalize_datatypes(row) for row in cursor.fetchall()]
+    return jsonify(days)
