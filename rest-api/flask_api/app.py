8f394f2b91c1f090f70b5766718e769a69d00818
StevenZuber <steven@getambassador.com>
2020-12-11 11:02:59 -0700

    stub out endpoint query for predictive model data fetch

--- rest-api/flask_api/app.py
+++ rest-api/flask_api/app.py
@@ -99,8 +99,8 @@ def fetch_state(state_id):
 @app.route('/cities/')
 def fetch_cities():
     sql = """
-        SELECT DISTINCT location_id, city, state, country 
-        FROM location_dim 
+        SELECT DISTINCT location_id, city, state, country
+        FROM location_dim
         WHERE city IS NOT NULL AND state IS NOT NULL AND country IS NOT NULL
         ORDER BY city, state, country;
         """
@@ -142,5 +142,25 @@ def normalize_datatypes(data):
     dc['hospitalization_rate'] = to_float(dc, 'hospitalization_rate')
     dc['latitude'] = to_float(dc, 'latitude')
     dc['longitude'] = to_float(dc, 'longitude')
-    
+
     return dc
+
+
+@app.route('/prediction/<int:location_id>')
+def fetch_prediction_data(location_id):
+    # placeholder query since my understanding is we need to update db columns/tables
+    # to include the regions described in the predictive model
+    sql = """
+        SELECT DISTINCT f.date_id, f.location_id, cases, recoveries, deaths,
+        cases_100k, testing_rate, hospitalization_rate,
+        date, year, month, day_of_week, day_of_month,
+        country, state, city, latitude, longitude, population
+        FROM covid_facts f JOIN date_dim d ON d.date_id = f.date_id
+        JOIN location_dim l ON l.location_id = f.location_id
+        WHERE f.location_id = %s;
+        """
+    db = connect_to_db()
+    cursor = db.cursor(cursor_factory=extras.DictCursor)
+    cursor.execute(sql, (location_id, ))
+    days = [normalize_datatypes(row) for row in cursor.fetchall()]
+    return jsonify(days)
